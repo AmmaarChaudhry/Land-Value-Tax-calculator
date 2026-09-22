8be0fc2b180f633cf2d955c0798bf8a53e69ccf2
Ammaar <ammaar.ch@outlook.com>
2026-09-22 19:16:54 +0100

    Basic GUI

--- LVT main.py
+++ LVT main.py
@@ -9,6 +9,37 @@ Plan:
 2) CSV HANDLING FUNCTION
 3) LVT CALCULATION FUNCTION
 """
+
+def csvQuery(postcode, county):
+    print("Querying...")
+    print(postcode + "," + county)
+    pd.set_option("display.max_columns", None)
+    dataFrame = pd.read_csv("pp-2025.csv", header=None)
+    columns = [
+        "transaction_id",
+        "price",
+        "date",
+        "postcode",
+        "property_type",
+        "new_build",
+        "duration",
+        "paon",
+        "saon",
+        "street",
+        "locality",
+        "town_city",
+        "district",
+        "county",
+        "ppd_category",
+        "record_status"
+    ]
+
+    dataFrame.columns = columns
+    head = dataFrame.head()
+    head = head.sort_values(by="price")
+    print(head)
+    
+
 def guiFunct():
     root = tk.Tk()
     root.title("Land Value Tax Calculator")
@@ -18,50 +49,24 @@ def guiFunct():
     tk.Label(root, text="Post Code:").grid(row=1, column=0)
     tk.Label(root, text="Region: ").grid(row=2, column=0)
 
-    button = tk.Button(root, text="Search...", width=25).grid(row=3)
+    pc_entry = tk.Entry(root)
+    county_entry = tk.Entry(root)
 
-    entry1 = tk.Entry(root)
-    entry2 = tk.Entry(root)
-
-    entry1.grid(row=1, column=1)
-    entry2.grid(row=2, column=1)
-
+    pc_entry.grid(row=1, column=1)
+    county_entry.grid(row=2, column=1)
+    
+    button = tk.Button(root, text="Search...", width=25, command=lambda: csvQuery(pc_entry.get(), county_entry.get()) ).grid(row=3)
 
     root.mainloop()
 
 
 
-pd.set_option("display.max_columns", None)
+
+guiFunct()
 
 
-dataFrame = pd.read_csv("pp-2025.csv", header=None)
 
-columns = [
-    "transaction_id",
-    "price",
-    "date",
-    "postcode",
-    "property_type",
-    "new_build",
-    "duration",
-    "paon",
-    "saon",
-    "street",
-    "locality",
-    "town_city",
-    "district",
-    "county",
-    "ppd_category",
-    "record_status"
-]
 
-dataFrame.columns = columns
-
-head = dataFrame.head()
-
-head = head.sort_values(by="price")
-
-print(head)
 
 #print(dataFrame.head())
 
