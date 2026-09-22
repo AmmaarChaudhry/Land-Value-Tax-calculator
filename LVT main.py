733db8ddc9918c35277effa51344cdf61ec39045
Ammaar <ammaar.ch@outlook.com>
2026-09-22 18:15:48 +0100

    Tkinter funct started

--- LVT main.py
+++ LVT main.py
@@ -1,10 +1,36 @@
 import pandas as pd
+import tkinter as tk
 
 #print(panda.__version__)
 
 """
-asd
+Plan:
+1) GUI FUNCTION
+2) CSV HANDLING FUNCTION
+3) LVT CALCULATION FUNCTION
 """
+def guiFunct():
+    root = tk.Tk()
+    root.title("Land Value Tax Calculator")
+
+    tk.Label(root, text="Enter property details and click search!").grid(row=0, column=0)
+
+    tk.Label(root, text="Post Code:").grid(row=1, column=0)
+    tk.Label(root, text="Region: ").grid(row=2, column=0)
+
+    button = tk.Button(root, text="Search...", width=25).grid(row=3)
+
+    entry1 = tk.Entry(root)
+    entry2 = tk.Entry(root)
+
+    entry1.grid(row=1, column=1)
+    entry2.grid(row=2, column=1)
+
+
+    root.mainloop()
+
+
+
 pd.set_option("display.max_columns", None)
 
 
